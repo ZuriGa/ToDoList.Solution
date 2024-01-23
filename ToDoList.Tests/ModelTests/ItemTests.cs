e31e0ab1538fd6cbea720a13afeafe9dbadbe7ad
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-01-23 15:35:51 -0800

    add testing for Find functionality, adding find method to items model

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -89,5 +89,21 @@ namespace ToDoList.Tests
       Assert.AreEqual(1, result);
     }
 
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      //Arrange
+      string description01 = "Walk the duck";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      //Act
+      Item result = Item.Find(2);
+      //Assert
+      Assert.AreEqual(newItem2, result);
+      
+
+    }
+
   }
 }
