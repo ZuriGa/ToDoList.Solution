d2aa7298be8877905f9e2d5e7ba52906e7fdf795
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-01-22 16:27:06 -0800

    creates list, and updated httpGet for route

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Item startedItem = new Item("Add first item to To Do List");
+      return View(startedItem);
+    }
+
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+  {
+    return View();
+  }
+
+  [HttpPost("/items")]
+  public ActionResult Create(string description)
+  {
+    Item myItem = new Item(description);
+    return View("Index", myItem);
+  }
+
+  }
+}
